54f584fcb6f774de029f6c29451e91e1f3d7c703
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2025-08-13 23:49:53 -0500

    Homework #1 Add Swiper carousel for music genres on home page
    Integrated Swiper.js into the project and updated the home page to display a carousel of music genres with images and descriptions. Updated styles and component imports to support Swiper, and replaced placeholder content with dynamic genre data.

--- retroMusicPCA/src/app/home/home.page.ts
+++ retroMusicPCA/src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+  genres = [
+    {
+      title: 'Pop latino / Folk pop',
+      image: 'https://imagenes.eltiempo.com/files/image_414_314/uploads/2020/03/13/5e6c460790e36.jpeg',
+      description: 'El pop latino con influencias de folk y música alternativa es un estilo que combina melodías suaves, arreglos acústicos y letras introspectivas. Artistas como Julieta Venegas lo han popularizado al fusionar instrumentos tradicionales, como el acordeón, con sonidos modernos, generando un sello distintivo que conecta con audiencias de toda Hispanoamérica.'
+    },
+    {
+      title: 'Pop rock / Rock latino',
+      image: 'https://i.pinimg.com/736x/cb/08/a4/cb08a49025adca9ed004c7d2b88fd96c.jpg',
+      description: 'El pop rock latino es un subgénero que fusiona la energía del rock con la accesibilidad melódica del pop, incorporando ritmos e instrumentos característicos de Latinoamérica. Maná ha sido uno de sus principales exponentes, logrando un sonido fresco que mezcla baladas románticas con temas de crítica social y ambiental.'
+    },
+    {
+      title: 'Balada pop / Trova',
+      image: 'https://i1.sndcdn.com/artworks-000317663007-slzxvg-t500x500.jpg',
+      description: 'La balada pop con influencias de trova es un estilo centrado en la narrativa lírica y las historias personales, combinando melodías suaves con arreglos sencillos que resaltan la voz y el mensaje. Ricardo Arjona ha hecho de este género un medio para reflexionar sobre la vida, el amor y las problemáticas sociales, manteniendo un estilo poético y directo.'
+    }
+  ]
   constructor() {}
 }
